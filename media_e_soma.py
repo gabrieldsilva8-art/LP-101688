1d02ef2ef4b4666a715479a53f804bc9559c4128
gabrieldsilva8-art <gabriel.d.silva8@ba.estudante.senai.br>
2026-02-24 16:44:55 -0300

    Add files via upload

---
+++ media_e_soma.py
@@ -0,0 +1,21 @@
+primeiro_numero = float (input('Digite seu primeiro numero'))
+
+segundo_numero = float (input('Digite seu segundo numero'))
+
+produto = (primeiro_numero * segundo_numero)
+
+soma = (primeiro_numero + segundo_numero)
+
+media = (primeiro_numero + segundo_numero) /2
+
+if primeiro_numero < segundo_numero:
+    print("Primeiro é menor")
+    print("segundo é maior")
+if primeiro_numero > segundo_numero:
+    print("Primeiro é menor")
+    print("segundo é maior")
+    
+print("soma", soma)
+print("media", media)
+print("produto", produto)
+    
